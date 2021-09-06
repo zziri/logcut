462eff63487ca431928ad2bcaf01edf7fcc2eae9
zziri <jihzang@naver.com>
2021-09-07 00:39:49 +0900

    setup project

---
+++ src/main/java/com/zziri/logcut/LogcutApplication.java
@@ -0,0 +1,13 @@
+package com.zziri.logcut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogcutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogcutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zziri/logcut/LogcutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zziri.logcut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogcutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
